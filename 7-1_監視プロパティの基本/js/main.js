ed392c032d919afabe6386e761a52a608f33c3d9
Chikuwa-Isobeyaki <kkisobe1213@icloud.com>
2022-08-09 21:22:45 +0900

    [Finish]7-2

---
+++ 7-1_監視プロパティの基本/js/main.js
@@ -0,0 +1,14 @@
+const app = Vue.createApp({
+  data: () =>({
+    message: 'Hello Vue.js!!'
+  }),
+  watch: {
+    message: function(newValue, oldValue) {
+      console.log('new: %s, old: %s', newValue, oldValue)
+    }
+  }
+})
+
+app.mount('#app')
+
+
