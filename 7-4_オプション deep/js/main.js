5984d272bc51ddf9776b75e81bdade2dffffcae6
Chikuwa-Isobeyaki <kkisobe1213@icloud.com>
2022-08-09 22:04:13 +0900

    [Finish]7-4

--- 7-4_オプション deep/js/main.js
+++ 7-4_オプション deep/js/main.js
@@ -1,7 +1,26 @@
 const app = Vue.createApp({
   data: () =>({
-    message: 'Hello Vue.js!!'
+    colors: [
+      { name: 'Red' },
+      { name: 'Green' },
+      { name: 'Blue' }
+    ]
   }),
+
+  watch: {
+    colors: {
+      handler: function(newValue, oldValue) {
+        console.log('Update!!')
+      },
+      deep: true
+    }
+  },
+
+  methods: {
+    onClick: function(event) {
+      this.colors[1].name = 'White'
+    }
+  }
 })
 
 app.mount('#app')
