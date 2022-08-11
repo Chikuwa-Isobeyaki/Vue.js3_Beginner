6e79e3c64f1f85ccc173eb0d0ace84706d02af35
Chikuwa-Isobeyaki <kkisobe1213@icloud.com>
2022-08-11 12:58:59 +0900

    [Finish]11-3

---
+++ 11-3_イベントオブジェクトの参照/js/main.js
@@ -0,0 +1,20 @@
+const app = Vue.createApp({
+  data: () =>({
+    counter: 0
+  }),
+
+  methods: {
+    clickHandler: function(event) {
+      this.counter++
+      console.log(event.target)
+      console.log(event.target.tagName)
+      console.log(event.target.innerHTML)
+      console.log(event.target.type)
+      console.log(event.target.id)
+    }
+  }
+})
+
+app.mount('#app')
+
+
