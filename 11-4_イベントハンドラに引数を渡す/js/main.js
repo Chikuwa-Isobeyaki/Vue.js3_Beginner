b88d9128f780e83da473421d0aaf9f28a0298ac8
Chikuwa-Isobeyaki <kkisobe1213@icloud.com>
2022-08-11 13:05:31 +0900

    [Finish]11-4

--- 11-4_イベントハンドラに引数を渡す/js/main.js
+++ 11-4_イベントハンドラに引数を渡す/js/main.js
@@ -1,16 +1,12 @@
 const app = Vue.createApp({
   data: () =>({
-    counter: 0
+    message: ''
   }),
 
   methods: {
-    clickHandler: function(event) {
-      this.counter++
-      console.log(event.target)
-      console.log(event.target.tagName)
-      console.log(event.target.innerHTML)
-      console.log(event.target.type)
-      console.log(event.target.id)
+    clickHandler: function(message) {
+      console.log(message)
+      this.message = message
     }
   }
 })
